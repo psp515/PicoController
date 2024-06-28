69ed90c88ce8cdd9b75a17c414da3b7bba92d210
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2024-06-28 20:20:30 +0200

    fix(pin fixes)

--- src/configuration/features/button_options.py
+++ src/configuration/features/button_options.py
@@ -1,6 +1,6 @@
 from configuration.base_options import BaseOptions
 
-DEFAULT_BUTTON_PIN = "14"
+DEFAULT_BUTTON_PIN = 14
 
 
 class ButtonOptions(BaseOptions):
@@ -8,15 +8,15 @@ class ButtonOptions(BaseOptions):
         super().__init__()
 
     @property
-    def button_pin(self) -> str:
+    def button_pin(self) -> int:
         try:
-            pin = self.manager.read_config(self.app_settings_name)["button"]["pin"]
+            pin = int(self.manager.read_config(self.app_settings_name)["button"]["pin"])
 
-            if pin == "":
+            if pin < 0 or pin >= 29:
                 return DEFAULT_BUTTON_PIN
 
             return pin
-        except KeyError:
+        except KeyError or ValueError:
             return DEFAULT_BUTTON_PIN
 
     def empty(self) -> bool:
--- src/configuration/features/strip_options.py
+++ src/configuration/features/strip_options.py
@@ -1,6 +1,6 @@
 from configuration.base_options import BaseOptions
 
-DEFAULT_BUTTON_PIN = "15"
+DEFAULT_BUTTON_PIN = 15
 
 
 class StripOptions(BaseOptions):
@@ -10,20 +10,23 @@ class StripOptions(BaseOptions):
         super().__init__()
 
     @property
-    def strip_pin(self):
+    def strip_pin(self) -> int:
         try:
-            pin = self.manager.read_config(self.app_settings_name)["strip"]["pin"]
+            pin = int(self.manager.read_config(self.app_settings_name)["strip"]["pin"])
 
-            if pin == "":
+            if pin < 0 or pin >= 29:
                 return DEFAULT_BUTTON_PIN
 
             return pin
-        except KeyError:
+        except KeyError or ValueError:
             return DEFAULT_BUTTON_PIN
 
     @property
-    def length(self):
-        return max(0, int(self.manager.read_config(self.app_settings_name)["strip"]["length"]))
+    def length(self) -> int:
+        try:
+            return max(0, int(self.manager.read_config(self.NAME)["length"]))
+        except ValueError:
+            return 0
 
     @length.setter
     def length(self, value: int):
